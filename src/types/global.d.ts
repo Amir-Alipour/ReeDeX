981967d19d57e35a54ee03bf3d96e0b683760a22
amir alipour <amir.alipour12127@gmail.com>
2024-02-01 20:20:28 +0330

    base types

---
+++ src/types/global.d.ts
@@ -0,0 +1,47 @@
+export { };
+
+declare global {
+    type Token = {
+        address: string
+        chainId: number
+        symbol: string
+        decimals: number
+        name: string
+        coinKey: string
+        logoURI: string
+        priceUSD: string
+    }
+
+    type Chain = {
+        key: string
+        chainType: string
+        name: string
+        coin: string
+        id: number
+        mainnet: boolean
+        logoURI: string
+        tokenlistUrl: string
+        multicallAddress: string
+        metamask: {
+            chainId: string
+            blockExplorerUrls: Array<string>
+            chainName: string
+            nativeCurrency: {
+                name: string
+                symbol: string
+                decimals: number
+            }
+            rpcUrls: Array<string>
+        }
+        nativeToken: {
+            address: string
+            chainId: number
+            symbol: string
+            decimals: number
+            name: string
+            coinKey: string
+            logoURI: string
+            priceUSD: string
+        }
+    }
+}
